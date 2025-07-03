512667dd99f711cd9de6212386358a448936dfc8
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2025-07-03 16:33:44 +0900

    초기세팅

---
+++ src/main/java/Phonesonal/PhoneBE/PhoneBeApplication.java
@@ -0,0 +1,13 @@
+package Phonesonal.PhoneBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Phonesonal/PhoneBE/PhoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package Phonesonal.PhoneBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
