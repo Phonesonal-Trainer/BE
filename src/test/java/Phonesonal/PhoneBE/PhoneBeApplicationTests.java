2023e8ecae65c785900c504335721e21169969f1
ica <irchrislee@gmail.com>
2025-08-13 02:07:54 +0900

    테스트 클래스 수정

--- src/test/java/Phonesonal/PhoneBE/PhoneBeApplicationTests.java
+++ src/test/java/Phonesonal/PhoneBE/PhoneBeApplicationTests.java
@@ -2,8 +2,10 @@ package Phonesonal.PhoneBE;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PhoneBeApplicationTests {
 
 	@Test
