1b8937c1a2d20ee54b34162e6e9193e24c47f5cf
ica <irchrislee@gmail.com>
2025-08-12 22:51:02 +0900

    ocr 구현완료

--- src/main/java/Phonesonal/PhoneBE/domain/Inbody.java
+++ src/main/java/Phonesonal/PhoneBE/domain/Inbody.java
@@ -1,6 +1,7 @@
 package Phonesonal.PhoneBE.domain;
 
 import Phonesonal.PhoneBE.domain.common.GoalPeriod;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,14 +24,14 @@ public class Inbody {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long inbodyId;
 
-    @SerializedName("weight")
+    @JsonProperty("weight")
     private BigDecimal weight;
 
-    @SerializedName("muscle_mass")
+    @JsonProperty("muscle_mass")
     private double muscleMass;   // 골격근량
 
-    @SerializedName("body_fat_percentage")
-    private double bodyFatMass;  // 체지방량
+    @JsonProperty("body_fat_percentage")
+    private double bodyFatPercentage;  // 체지방량
 
     // 사용자 기준으로 소유자 명시
     @ManyToOne(fetch = FetchType.LAZY)
@@ -41,8 +42,6 @@ public class Inbody {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "goal_period_id")
     private GoalPeriod goalPeriod;
-
-    private LocalDate date;
 
     @Column(length = 255)
     private String imageUrl;
