51c781d891d5b9cc99efc0c3c332223bab022ea5
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2025-08-14 22:06:08 +0900

    버그수정

--- src/main/java/Phonesonal/PhoneBE/web/dto/Exercise/request/CreateMultipleUserExerciseRequestDTO.java
+++ src/main/java/Phonesonal/PhoneBE/web/dto/Exercise/request/CreateMultipleUserExerciseRequestDTO.java
@@ -1,12 +1,16 @@
 package Phonesonal.PhoneBE.web.dto.Exercise.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateMultipleUserExerciseRequestDTO {
     private List<Long> exerciseIds;
 }
