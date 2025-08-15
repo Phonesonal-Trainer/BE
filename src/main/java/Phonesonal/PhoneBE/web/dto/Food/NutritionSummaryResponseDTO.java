4c155c14e7086aabfc2d21a532a6502cf1337f59
seoyoungleeme <okcathy@ewhain.net>
2025-08-15 22:02:20 +0900

    식단 total calorie 추가

--- src/main/java/Phonesonal/PhoneBE/web/dto/Food/NutritionSummaryResponseDTO.java
+++ src/main/java/Phonesonal/PhoneBE/web/dto/Food/NutritionSummaryResponseDTO.java
@@ -9,10 +9,14 @@ import java.time.LocalDate;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class NutritionSummaryResponseDTO {
     private LocalDate date;
     private MealSummary summary;
+
+    private int plannedTotalCalorie; // RecommendMeal의 아침/점심/저녁/간식 전부(complete 여부 상관없음)
+    private int actualTotalCalorie;  // RecommendMeal 중 complete + UserMeal 전부
 
     @Data
     @NoArgsConstructor
